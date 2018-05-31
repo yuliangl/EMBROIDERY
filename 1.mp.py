f5122f01f9b862bf6c548b6e938d860c4fcdbacf
yuliang <yigeren010@gmail.com>
2018-05-31 13:42:22 +0900

    test git

---
+++ 1.mp.py
@@ -0,0 +1,7 @@
+name = input("please write your name:")
+tele = input("please tell me your number ")
+
+print("-----------------")
+print("name:%s"%name)
+print("tele:%s"%tele)
+print("-----------------")
